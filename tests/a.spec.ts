dc47fdd535e4c612e6830aec5bb82e1422d36f9d
Jacek Kruszewski <jacek.kruszewski@dietly.pl>
2023-09-28 10:41:18 +0200

    domena

--- tests/a.spec.ts
+++ tests/a.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 for (let i = 0; i < 100; ++i) {
-  const url = `https://playwright.dev/?${i + 1}`;
+  const url = `https://dietly.pl/?${i + 1}`;
   test(url, async ({ page }) => {
     const response = await page.goto(url);
     expect(response!.status()).toBe(200);
