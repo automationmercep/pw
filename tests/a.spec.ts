7aee2753bfacc99f45d74856c3eac836cdfcfb0c
Jacek Kruszewski <jacek.kruszewski@dietly.pl>
2023-09-28 11:22:24 +0200

    workflow

--- tests/a.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-for (let i = 0; i < 100; ++i) {
-  const url = 'https://dietly.pl/';
-  test(`Test numer: '${i + 1 } ` + url, async ({ page }) => {
-    const response = await page.goto(url);
-    expect(response!.status()).toBe(200);
-  });
-}
